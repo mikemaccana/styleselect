a70fda1d56d7420854de067521cb9efefe845647
Mike MacCana <mike.maccana@gmail.com>
2014-09-19 11:51:14 +0100

    Replace unused unused [xy] range from regex with just 'x'

--- js/visualselect.js
+++ js/visualselect.js
@@ -15,7 +15,7 @@ define(function() {
 		};
 
 		var makeUUID = function(){
-			var UUID = 'vs-xxxx-xxxx-xxxx-xxxx-xxxx'.replace(/[xy]/g, function(c) {
+			var UUID = 'vs-xxxx-xxxx-xxxx-xxxx-xxxx'.replace(/x/g, function(c) {
 				var r = Math.random() * 16|0, v = c == 'x'? r : r&0x3|0x8;
 				return v.toString(16);
 			})
