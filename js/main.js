e708eaa8c2cea8b5574815df864689e61611ceae
Mike MacCana <mike.maccana@gmail.com>
2014-10-27 13:30:35 +0000

    More doc tweaks

--- js/main.js
+++ js/main.js
@@ -3,20 +3,16 @@ define(['styleselect'], function(styleSelect){
 
   styleSelect('select');
 
+  // Rest of code is for demonstration purposes...
   var selectBox = document.querySelector('select')
 
   var demoBinding = new Ractive({
     el: document.querySelector('p.result'),
-    template: 'Value of select box is currently {{ value }}',
+    template: 'Value of real select box is currently {{ value }}',
     data: {value: selectBox.value}
   })
 
   selectBox.addEventListener('change', function(){
     demoBinding.set('value', selectBox.value)
-    log('styleselect also triggers change events on the real select boxes')
   })
-
-  log('Select box above should now by styled')
-  log("Check the value of the real select box with: document.querySelector('select').value")
-  log("Check the text of the real select box with: var select = document.querySelector('select'); select.options[select.selectedIndex].text;")
 })
